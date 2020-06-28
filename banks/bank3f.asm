5151a1a4ffd7d913185b2e1fe73ab55169e45a03
libjet <libj3t@gmail.com>
2020-06-28 20:01:25 +0100

    Disassemble OTID Editor

--- banks/bank3f.asm
+++ banks/bank3f.asm
@@ -17,363 +17,6 @@ Call_03f_452b::
 	nop
 
 INCLUDE "engine/debug/debug_menu_overworld.asm"
-
-Call_03f_4797:
-.loop:
-	call JoyTextDelay_ForcehJoyDown
-	ld a, c
-	bit 6, a
-	jr nz, jr_03f_47b5
-	bit 7, a
-	jr nz, jr_03f_47c4
-	bit 5, a
-	jr nz, jr_03f_47d3
-	bit 4, a
-	jr nz, jr_03f_47dd
-	bit 1, a
-	jr nz, jr_03f_47e9
-	bit 0, a
-	jr nz, jr_03f_47eb
-	jr .loop
-
-jr_03f_47b5:
-	ld hl, wDebugClockCurrentOption
-	ld a, [hl]
-	cp $fb
-	jr z, jr_03f_47c0
-
-	inc [hl]
-	xor a
-	ret
-
-jr_03f_47c0:
-	ld [hl], 1
-	xor a
-	ret
-
-jr_03f_47c4:
-	ld hl, wDebugClockCurrentOption
-	ld a, [hl]
-	cp $01
-	jr z, jr_03f_47cf
-	dec [hl]
-	xor a
-	ret
-
-jr_03f_47cf:
-	ld [hl], $fb
-	xor a
-	ret
-
-jr_03f_47d3:
-	ld hl, wDebugClockCurrentOptionBackup
-	dec [hl]
-	jr nz, jr_03f_47db
-	ld [hl], $63
-jr_03f_47db:
-	xor a
-	ret
-
-jr_03f_47dd:
-	ld hl, wDebugClockCurrentOptionBackup
-	inc [hl]
-	cp $64
-	jr c, jr_03f_47e7
-	ld [hl], 1
-jr_03f_47e7:
-	xor a
-	ret
-
-jr_03f_47e9:
-	scf
-	ret
-
-jr_03f_47eb:
-	ld a, 1
-	and a
-	ret
-
-Call_03f_47ef:
-	ld hl, wOptions
-	ld a, [hl]
-	push af
-	set 4, [hl]
-	ldh a, [hBGMapMode]
-	push af
-	xor a
-	ldh [hBGMapMode], a
-	ld a, [wDebugClockCurrentOption]
-	ld [wd143], a
-	call GetItemName
-
-	ld hl, .unkData_03f_4813
-	call PrintText
-	pop af
-	ldh [hBGMapMode], a
-	pop af
-	ld [wOptions], a
-	ret
-
-.unkData_03f_4813:
-	text "ばんごう@"
-	text_decimal $d0c5, $1, $3 ; TEMP
-	text ""
-	line "@"
-	text_ram $cf87 ; TEMP
-	text "  ×@"
-	text_decimal $d0c6, $1, $2 ; TEMP
-	text_end
-
-unk_03f_482d:
-	ld hl, .unkData_03f_4866
-	call MenuTextbox
-.asm_4833:
-	call UpdateTime
-	ld a, 4
-	ld hl, $5861
-	rst FarCall
-	ld hl, $c4e5
-	ld de, wBugContestMinsRemaining
-	ld bc, $8102
-	call PrintNum
-	ld hl, $c4e8
-	ld de, wBugContestSecsRemaining
-	lb bc, $81, 2
-	call PrintNum
-	call WaitBGMap
-	call GetJoypad
-	ldh a, [hJoyPressed]
-	and $03
-	jr z, .asm_4833
-	call ExitMenu
-	ld a, 0
-	ret
-
-.unkData_03f_4866:
-	text "たいかい のこりじかん"
-	done
-
-.asm_4873:
-	ld a, 0
-	ret
-
-unk_03f_4876:
-	call Call_03f_48e4
-	jr c, .asm_4890
-	ld a, [wMenuCursorY]
-	dec a
-	ld hl, .unkData_03f_4884
-	rst JumpTable
-	ret
-
-.unkData_03f_4884:
-	dw $48c4
-	dw $48d5
-	dw $48a1
-	dw $4893
-	dw $489a
-	dw $48cf
-
-.asm_4890:
-	ld a, 0
-	ret
-
-unk_03f_4893:
-	ld hl, wd55c
-	set 7, [hl]
-	jr jr_03f_48a7
-
-unk_03f_489a:
-	ld hl, wd55c
-	res 7, [hl]
-	jr jr_03f_48a7
-
-unk_03f_48a1:
-	ld a, $3f
-	ld hl, $4000
-	rst FarCall
-
-jr_03f_48a7:
-	ld a, $23
-	ld hl, $440e
-	rst FarCall
-	ld a, $23
-	ld hl, $43b9
-	rst FarCall
-	ld b, 9
-	call GetSGBLayout
-	ld a, $23
-	ld hl, $4403
-	rst FarCall
-	call UpdateTimePals
-	ld a, 1
-	ret
-
-unk_03f_48c4:
-	call Function1e7c
-	ld hl, wd55c
-	res 0, [hl]
-	ld a, 1
-	ret
-
-unk_03f_48cf:
-	call Function1e82
-	ld a, 1
-	ret
-
-unk_03f_48d5:
-	call Call_03f_48db
-	ld a, 1
-	ret
-
-Call_03f_48db:
-	call Function1e7c
-	ld hl, wd55c
-	set 0, [hl]
-	ret
-
-Call_03f_48e4:
-	ld hl, unkData_03f_490a
-	call LoadMenuHeader
-	call Call_03f_48f7
-	ld [wMenuCursorBuffer], a
-	call VerticalMenu
-	call CloseWindow
-	ret
-
-Call_03f_48f7:
-	ld a, [wEnteredMapFromContinue]
-	bit 0, a
-	ld a, 3
-	ret nz
-	ld hl, wd55c
-	bit 0, [hl]
-	ld a, 1
-	ret nz
-	ld a, 2
-	ret
-
-unkData_03f_490a:
-	dr $fc90a,$fc954
-
-unk_03f_4954:
-	xor a
-	ldh [hMapAnims], a
-	call LoadStandardMenuHeader
-	call ClearSprites
-
-	ld a, 0
-	ld [wCurPartyMon], a
-	ld a, $24
-	ld hl, $6398
-	rst FarCall
-
-	ld a, e
-	ld [wMenuSelection], a
-	call CloseSubmenu
-	ld a, [wMenuSelection]
-	cp -1
-	jr z, .asm_499d
-	ld a, [wMenuSelection]
-	cp -1
-	jr z, .asm_499d
-	cp $1c
-	jr nc, .asm_499d
-
-	ld [wDefaultSpawnpoint], a
-	ld hl, wVramState
-	set 6, [hl]
-	ldh a, [hROMBank]
-	ld hl, $4a5b
-	call FarQueueScript
-	ld de, $27
-	call PlaySFX
-	call DelayFrame
-	ld a, 4
-	ret
-
-.asm_499d:
-	ld a, 0
-	ret
-
-unkData_03f_49a0:
-	dr $fc9a0,$fc9b8
-
-unk_03f_49b8:
-	dr $fc9b8,$fc9c4
-
-Call_03f_49c4:
-	ld hl, unkData_03f_49cd
-	call GetNthString
-	ld d, h
-	ld e, l
-	ret
-
-unkData_03f_49cd:
-	dr $fc9cd,$fca67
-
-Call_03f_4a67:
-	ld hl, unkData_03f_4a6e
-	call Call_03f_46df
-	ret
-
-unkData_03f_4a6e:
-	dr $fca6e,$fca95
-
-Call_03f_4a95:
-	dr $fca95,$fcaea
-
-Call_03f_4aea:
-	dr $fcaea,$fcb12
-
-Call_03f_4b12:
-	dr $fcb12,$fcb57
-
-Call_03f_4b57:
-	dr $fcb57,$fcb62
-
-Call_03f_4b62:
-	dr $fcb62,$fcb91
-
-unk_03f_4b91:
-	dr $fcb91,$fcbaf
-
-Call_03f_4baf:
-	dr $fcbaf,$fcbdb
-
-Call_03f_4bdb:
-	dr $fcbdb,$fcc07
-
-Call_03f_4c07:
-	dr $fcc07,$fcc65
-
-unk_03f_4c65:
-	dr $fcc65,$fcc7a
-
-unk_03f_4c7a:
-	dr $fcc7a,$fccc0
-
-Call_03f_4cc0:
-	dr $fccc0,$fccd1
-
-unk_03f_4cd1:
-	dr $fccd1,$fcd51
-
-Call_03f_4d51:
-	dr $fcd51,$fcd59
-
-unk_03f_4d59:
-	dr $fcd59,$fcd81
-
-Call_03f_4d81:
-	dr $fcd81,$fcda7
-
-unk_03f_4da7:
-	dr $fcda7,$fcebb
-
-Call_03f_4ebb:
-	dr $fcebb,$fcee2
-
 INCLUDE "engine/debug/debug_fight_menu.asm"
 
 unkData_03f_57d4:
@@ -391,8 +34,26 @@ Jump_03f_5851:
 Jump_03f_585e:
 	dr $fd85e,$fd868
 
-Call_03f_5868:
-	dr $fd868,$fd897
+Call_03f_5868::
+	hlcoord 1, 0
+	ld b, 2
+	ld c, 9
+	call ClearBox
+	hlcoord 1, 1
+	ld de, wCurPartySpecies
+	lb bc, PRINTNUM_LEADINGZEROS | 1, 3
+	call PrintNum
+	inc hl
+	push hl
+	ld a, [wCurPartySpecies]
+	ld [wTempSpecies], a
+	call GetPokemonName
+	pop hl
+	call PlaceString
+	ld a, [wTempSpecies]
+	ld [wCurSpecies], a
+	call GetBaseData
+	ret
 
 Jump_03f_5897:
 	dr $fd897,$fd8d2
@@ -404,7 +65,11 @@ Jump_03f_58dc:
 	dr $fd8dc,$fd8e3
 
 Call_03f_58e3:
-	dr $fd8e3,$fd8f0
+	hlcoord 1, 3
+	ld de, wCurPartyLevel
+	lb bc, PRINTNUM_LEADINGZEROS | 1, 3
+	call PrintNum
+	ret
 
 Call_03f_58f0:
 	dr $fd8f0,$fd93e
@@ -424,8 +89,60 @@ Jump_03f_5a42:
 Call_03f_5a51:
 	dr $fda51,$fda77
 
-Call_03f_5a77:
-	dr $fda77,$fdae4
+Call_03f_5a77::
+	hlcoord 12, 0
+	ld b, 18
+	ld c, 8
+	call ClearBox
+	hlcoord 13, 1
+	ld de, unkData_03f_5ac9
+	call PlaceString
+
+	ld b, $a
+	ld hl, wd010
+	ld a, [wddf0]
+.asm_5a92
+	ld [hli], a
+	dec b
+	jr nz, .asm_5a92
+
+	ld a, [wddee]
+	ld [hli], a
+	ld a, [wddef]
+	ld [hl], a
+	ld hl, wd00f
+	ld de, wd029
+	ld b, 1
+	ld a, 12
+	call Predef
+
+	hlcoord 17, 1
+	ld de, wd029
+	ld b, 6
+.asm_5ab3
+	push bc
+	push de
+	push hl
+	lb bc, PRINTNUM_LEADINGZEROS | 2, 3
+	call PrintNum
+	pop hl
+	ld bc, $28
+	add hl, bc
+	pop de
+	inc de
+	inc de
+	pop bc
+	dec b
+	jr nz, .asm_5ab3
+	ret
+
+unkData_03f_5ac9:
+	db "たいリき"
+	next "<KOUGEKI>"
+	next "ぼうぎょ"
+	next "すばやさ"
+	next "とくこう"
+	next "とくぼう@"
 
 Jump_03f_5ae4:
 	dr $fdae4,$fdb65
--- engine/debug/debug_menu_overworld.asm
+++ engine/debug/debug_menu_overworld.asm
@@ -12,62 +12,50 @@ unkData_03f_454e:
 	dw unk_03f_4556
 
 unk_03f_4556:
-	dw $46a1, unkData_03f_45aa
-	dw $46c8, unkData_03f_45cf
-	dw $4bd5, unkData_03f_45ae
-	dw $4873, unkData_03f_45d8
-	dw $4876, unkData_03f_45dc
-	dw $4da7, unkData_03f_45b6
-	dw $4dae, unkData_03f_45bb
-	dw $4db5, unkData_03f_45c0
-	dw $4dbc, unkData_03f_45c5
-	dw $4932, unkData_03f_45ca
-	dw $4954, unkData_03f_45b2
-	dw $4ba0, unkData_03f_45d3
-	dw $4a86, $45e2
-	dw $4b91, $45e7
-	dw $4c65, $45f7
-	dw $4ba9, $45eb
-	dw $4c7a, $45f3
-	dw $4718, $45fc
-	dw $482d, $4600
-	dw $4cd1, $4605
-	dw $4d59, $460a
+	dw unk_03f_46a1, unkData_03f_45aa
+	dw unk_03f_46c8, unkData_03f_45cf
+	dw unk_03f_4bd5, unkData_03f_45ae
+	dw unk_03f_4873, unkData_03f_45d8
+	dw unk_03f_4876, unkData_03f_45dc
+	dw unk_03f_4da7, unkData_03f_45b6
+	dw unk_03f_4dae, unkData_03f_45bb
+	dw unk_03f_4db5, unkData_03f_45c0
+	dw unk_03f_4dbc, unkData_03f_45c5
+	dw unk_03f_4932, unkData_03f_45ca
+	dw unk_03f_4954, unkData_03f_45b2
+	dw unk_03f_4ba0, unkData_03f_45d3
+	dw OTID_Editor,  unkData_03f_45e2
+	dw unk_03f_4b91, unkData_03f_45e7
+	dw unk_03f_4c65, unkData_03f_45f7
+	dw unk_03f_4ba9, unkData_03f_45eb
+	dw unk_03f_4c7a, unkData_03f_45f3
+	dw unk_03f_4718, unkData_03f_45fc
+	dw unk_03f_482d, unkData_03f_4600
+	dw unk_03f_4cd1, unkData_03f_4605
+	dw unk_03f_4d59, unkData_03f_460a
 
-unkData_03f_45aa:
-	db "とじる@" ; Cancel
-unkData_03f_45ae:
-	db "ゲーム@" ; Game
-unkData_03f_45b2:
-	db "ワープ@" ; Warp
-unkData_03f_45b6:
-	db "テスト1@" ; Test 1
-unkData_03f_45bb:
-	db "テスト2@" ; Test 2
-unkData_03f_45c0:
-	db "テスト3@" ; Test 3
-unkData_03f_45c5:
-	db "テスト4@" ; Test 4
-unkData_03f_45ca:
-	db "かいふく@" ; Recovery
-unkData_03f_45cf:
-	db "つぎ▶@" ; Next
-unkData_03f_45d3:
-	db "パソコン@" ; PC
-unkData_03f_45d8:
-	db "キャラ@" ; Character
-unkData_03f_45dc:
-	db "ツールギア@" ; Pokegear
-	db "じっけん@" ; Experiment​
-	db "つくる@" ; Build a Pokemon
-	db "フロア@" ; Floor
-	db "たまご@" ; Egg
-	db "きろく@" ; Record
-	db "タイマー@" ; Timer
-	db "どうぐ@" ; Item
-	db "むしとり@" ; Bug Catching
-	db "こづくり@" ; Breeding
-	db "うまれる@" ; Hatch
+unkData_03f_45aa: db "とじる@" ; Cancel
+unkData_03f_45ae: db "ゲーム@" ; Game
+unkData_03f_45b2: db "ワープ@" ; Warp
+unkData_03f_45b6: db "テスト1@" ; Test 1
+unkData_03f_45bb: db "テスト2@" ; Test 2
+unkData_03f_45c0: db "テスト3@" ; Test 3
+unkData_03f_45c5: db "テスト4@" ; Test 4
+unkData_03f_45ca: db "かいふく@" ; Recovery
+unkData_03f_45cf: db "つぎ▶@" ; Next
+unkData_03f_45d3: db "パソコン@" ; PC
+unkData_03f_45d8: db "キャラ@" ; Character
+unkData_03f_45dc: db "ツールギア@" ; Pokegear
+unkData_03f_45e2: db "じっけん@" ; Experiment​ (OT ID Editor)
+unkData_03f_45e7: db "つくる@" ; Build a Pokemon
+unkData_03f_45eb: db "フロア@" ; Floor
+unkData_03f_45ef: db "たまご@" ; Egg
+unkData_03f_45f3: db "きろく@" ; Record
+unkData_03f_45f7: db "タイマー@" ; Timer
+unkData_03f_45fc: db "どうぐ@" ; Item
+unkData_03f_4600: db "むしとり@" ; Bug Catching
+unkData_03f_4605: db "こづくり@" ; Breeding
+unkData_03f_460a: db "うまれる@" ; Hatch
 
 unk_03f_460f:
 	db 7
@@ -94,12 +82,14 @@ unk_03f_462a:
 
 	ld a, 0
 	ld [wWhichIndexSet], a
+
+unk_03f_463e:
 	call UpdateTimePals
 	call UpdateSprites
 	ld a, [wcfbf]
 	ld [wMenuCursorBuffer], a
 	call DoNthMenu
-	jr c, jr_03f_467c
+	jr c, unk_03f_467c
 
 	ld a, [wMenuCursorBuffer]
 	ld [wcfbf], a
@@ -122,16 +112,16 @@ jr_03f_466d:
 	ret
 
 unk_03f_4672:
-	dw $463e
-	dw $467c
-	dw $467f
-	dw $4685
-	dw $468e
+	dw unk_03f_463e
+	dw unk_03f_467c
+	dw unk_03f_467f
+	dw unk_03f_4685
+	dw unk_03f_468e
 
-jr_03f_467c:
+unk_03f_467c:
 	call CloseWindow
 
-jr_03f_467f:
+unk_03f_467f:
 	push af
 	call CloseText
 	pop af
@@ -141,7 +131,9 @@ unk_03f_4685:
 	call ExitMenu
 	ld a, $80
 	ldh [hMenuReturn], a
-	jr jr_03f_467f
+	jr unk_03f_467f
+
+unk_03f_468e:
 	call ExitMenu
 	ld hl, wQueuedScriptAddr
 	ld a, [hli]
@@ -151,7 +143,9 @@ unk_03f_4685:
 	rst FarCall
 	ld a, $80
 	ldh [hMenuReturn], a
-	jr jr_03f_467f
+	jr unk_03f_467f
+
+unk_03f_46a1:
 	ld a, 1
 	ret
 
@@ -166,7 +160,7 @@ Call_03f_46a4:
 	xor a
 .asm_46b4
 	ld [wWhichIndexSet], a
-	jr .asm_46d6
+	jr unk_03f_46d6
 
 .asm_46b9
 	ld a, [wWhichIndexSet]
@@ -176,9 +170,9 @@ Call_03f_46a4:
 	ld a, 2
 .asm_46c3
 	ld [wWhichIndexSet], a
-	jr .asm_46d6
+	jr unk_03f_46d6
 
-.asm_46c8
+unk_03f_46c8:
 	ld a, [wWhichIndexSet]
 	and a
 	jr z, .asm_46d1
@@ -191,7 +185,7 @@ Call_03f_46a4:
 .asm_46d3
 	ld [wWhichIndexSet], a
 
-.asm_46d6
+unk_03f_46d6:
 	ld de, $06
 	call PlaySFX
 	ld a, 0
@@ -287,3 +281,635 @@ Call_03f_4748:
 	text "を "
 	line "りュックにいれました"
 	done
+
+Call_03f_4797:
+.loop:
+	call JoyTextDelay_ForcehJoyDown
+	ld a, c
+	bit 6, a
+	jr nz, jr_03f_47b5
+	bit 7, a
+	jr nz, jr_03f_47c4
+	bit 5, a
+	jr nz, jr_03f_47d3
+	bit 4, a
+	jr nz, jr_03f_47dd
+	bit 1, a
+	jr nz, jr_03f_47e9
+	bit 0, a
+	jr nz, jr_03f_47eb
+	jr .loop
+
+jr_03f_47b5:
+	ld hl, wDebugClockCurrentOption
+	ld a, [hl]
+	cp $fb
+	jr z, jr_03f_47c0
+
+	inc [hl]
+	xor a
+	ret
+
+jr_03f_47c0:
+	ld [hl], 1
+	xor a
+	ret
+
+jr_03f_47c4:
+	ld hl, wDebugClockCurrentOption
+	ld a, [hl]
+	cp $01
+	jr z, jr_03f_47cf
+	dec [hl]
+	xor a
+	ret
+
+jr_03f_47cf:
+	ld [hl], $fb
+	xor a
+	ret
+
+jr_03f_47d3:
+	ld hl, wDebugClockCurrentOptionBackup
+	dec [hl]
+	jr nz, jr_03f_47db
+	ld [hl], $63
+jr_03f_47db:
+	xor a
+	ret
+
+jr_03f_47dd:
+	ld hl, wDebugClockCurrentOptionBackup
+	inc [hl]
+	cp $64
+	jr c, jr_03f_47e7
+	ld [hl], 1
+jr_03f_47e7:
+	xor a
+	ret
+
+jr_03f_47e9:
+	scf
+	ret
+
+jr_03f_47eb:
+	ld a, 1
+	and a
+	ret
+
+Call_03f_47ef:
+	ld hl, wOptions
+	ld a, [hl]
+	push af
+	set 4, [hl]
+	ldh a, [hBGMapMode]
+	push af
+	xor a
+	ldh [hBGMapMode], a
+	ld a, [wDebugClockCurrentOption]
+	ld [wd143], a
+	call GetItemName
+
+	ld hl, .unkData_03f_4813
+	call PrintText
+	pop af
+	ldh [hBGMapMode], a
+	pop af
+	ld [wOptions], a
+	ret
+
+.unkData_03f_4813:
+	text "ばんごう@"
+	text_decimal $d0c5, $1, $3 ; TEMP
+	text ""
+	line "@"
+	text_ram $cf87 ; TEMP
+	text "  ×@"
+	text_decimal $d0c6, $1, $2 ; TEMP
+	text_end
+
+unk_03f_482d:
+	ld hl, .unkData_03f_4866
+	call MenuTextbox
+.asm_4833:
+	call UpdateTime
+	ld a, 4
+	ld hl, $5861
+	rst FarCall
+	ld hl, $c4e5
+	ld de, wBugContestMinsRemaining
+	lb bc, $81, 2
+	call PrintNum
+	ld hl, $c4e8
+	ld de, wBugContestSecsRemaining
+	lb bc, $81, 2
+	call PrintNum
+	call WaitBGMap
+	call GetJoypad
+	ldh a, [hJoyPressed]
+	and $03
+	jr z, .asm_4833
+	call ExitMenu
+	ld a, 0
+	ret
+
+.unkData_03f_4866:
+	text "たいかい のこりじかん"
+	done
+
+unk_03f_4873:
+	ld a, 0
+	ret
+
+unk_03f_4876:
+	call Call_03f_48e4
+	jr c, .asm_4890
+	ld a, [wMenuCursorY]
+	dec a
+	ld hl, .unkData_03f_4884
+	rst JumpTable
+	ret
+
+.unkData_03f_4884:
+	dw unk_03f_48c4
+	dw unk_03f_48d5
+	dw unk_03f_48a1
+	dw unk_03f_4893
+	dw unk_03f_489a
+	dw unk_03f_48cf
+
+.asm_4890:
+	ld a, 0
+	ret
+
+unk_03f_4893:
+	ld hl, wd55c
+	set 7, [hl]
+	jr jr_03f_48a7
+
+unk_03f_489a:
+	ld hl, wd55c
+	res 7, [hl]
+	jr jr_03f_48a7
+
+unk_03f_48a1:
+	ld a, $3f
+	ld hl, $4000
+	rst FarCall
+
+jr_03f_48a7:
+	ld a, $23
+	ld hl, $440e
+	rst FarCall
+	ld a, $23
+	ld hl, $43b9
+	rst FarCall
+	ld b, 9
+	call GetSGBLayout
+	ld a, $23
+	ld hl, $4403
+	rst FarCall
+	call UpdateTimePals
+	ld a, 1
+	ret
+
+unk_03f_48c4:
+	call Function1e7c
+	ld hl, wd55c
+	res 0, [hl]
+	ld a, 1
+	ret
+
+unk_03f_48cf:
+	call Function1e82
+	ld a, 1
+	ret
+
+unk_03f_48d5:
+	call Call_03f_48db
+	ld a, 1
+	ret
+
+Call_03f_48db:
+	call Function1e7c
+	ld hl, wd55c
+	set 0, [hl]
+	ret
+
+Call_03f_48e4:
+	ld hl, MenuHeader_03f_490a
+	call LoadMenuHeader
+	call Call_03f_48f7
+	ld [wMenuCursorBuffer], a
+	call VerticalMenu
+	call CloseWindow
+	ret
+
+Call_03f_48f7:
+	ld a, [wEnteredMapFromContinue]
+	bit 0, a
+	ld a, 3
+	ret nz
+	ld hl, wd55c
+	bit 0, [hl]
+	ld a, 1
+	ret nz
+	ld a, 2
+	ret
+
+MenuHeader_03f_490a:
+	db MENU_BACKUP_TILES ; flags
+	db 0, 0, 14, 7
+	dw MenuData_03f_4912
+	db 1 ; default option
+
+MenuData_03f_4912:
+	db STATICMENU_CURSOR ; flags
+	db 6
+	db "とけい@" ; Clock
+	db "ざひょう@" ; Coordinates
+	db "アジャスト@" ; Adjust
+	db "60びょう@" ; 60 Seconds
+	db "24じかん@" ; 24 Hours
+	db "けす@" ; Erase
+
+unk_03f_4932:
+	ld a, 2
+	call Predef
+	ld hl, unkData_03f_4940
+	call MenuTextboxBackup
+	ld a, 0
+	ret
+
+unkData_03f_4940:
+	text "#の たいリょくを"
+	line "かいふくしました"
+	prompt
+
+unk_03f_4954:
+	xor a
+	ldh [hMapAnims], a
+	call LoadStandardMenuHeader
+	call ClearSprites
+	ld a, 0
+	ld [wCurPartyMon], a
+	ld a, $24
+	ld hl, $6398
+	rst FarCall
+	ld a, e
+	ld [wMenuSelection], a
+	call CloseSubmenu
+	ld a, [wMenuSelection]
+	cp -1
+	jr z, .asm_499d
+	ld a, [wMenuSelection]
+	cp -1
+	jr z, .asm_499d
+	cp $1c
+	jr nc, .asm_499d
+	ld [wDefaultSpawnpoint], a
+	ld hl, wVramState
+	set 6, [hl]
+	ldh a, [hROMBank]
+	ld hl, unk_03f_4a5b
+	call FarQueueScript
+	ld de, $27
+	call PlaySFX
+	call DelayFrame
+	ld a, 4
+	ret
+
+.asm_499d:
+	ld a, 0
+	ret
+
+unkData_03f_49a0:
+	db MENU_BACKUP_TILES ; flags
+	db 01, 01 ; start coords
+	db 10, 11 ; end coords
+	dw MenuData_03f_49a8
+	db 1 ; default option
+
+MenuData_03f_49a8:
+	db 0 ; flags
+	db 4, 0 ; rows, columns
+	db 1 ; spacing
+	dba unkData_03f_4a43
+	dba unk_03f_49b8
+	db $00
+	dw $0000
+	db $00
+	dw $0000
+
+unk_03f_49b8:
+	push de
+	ld a, [wMenuSelection]
+	call Call_03f_49c4
+	pop hl
+	call PlaceString
+	ret
+
+Call_03f_49c4:
+	ld hl, unkData_03f_49cd
+	call GetNthString
+	ld d, h
+	ld e, l
+	ret
+
+; Warp destinations
+unkData_03f_49cd:
+	db "じぶんのうち@" ; My home
+	db "マサラ@" ; Pallet Town
+	db "トキワ@" ; Viridian City
+	db "ニビ@" ; Pewter City
+	db "ハナダ@" ; Cerulean City
+	db "イワヤマトンネルまえ@" ; Rock Tunnel Entrance
+	db "クチバ@" ; Vermilion City
+	db "シオン@" ; Lavender Town
+	db "ヤマブキ@" ; Saffron City
+	db "タマムシ@" ; Celadon City
+	db "セキチク@" ; Fuchsia City
+	db "グレン@" ; Cinnabar Island
+
+	db "ワカバ@" ; New Bark Town
+	db "ヨシノ@" ; Cherrygrove City
+	db "キキョウ@" ; Violet City
+	db "ヒワダ@" ; Azalea Town
+	db "タンバ@" ; Cianwood City
+	db "コガネ@" ; Goldenrod City
+	db "アサギ@" ; Olivine City
+	db "エンジュ@" ; Ecruteak City
+	db "チョウジ@" ; Mahogany Town
+	db "イカリの みずうみ@" ; Lake of Rage
+	db "フスべ@" ; Blackthorn City
+	db "シロガネ@" ; Mt. Silver
+
+unkData_03f_4a43:
+	db SPAWN_NEW_BARK, SPAWN_CHERRYGROVE, SPAWN_VIOLET, SPAWN_AZALEA, SPAWN_CIANWOOD, SPAWN_GOLDENROD, SPAWN_OLIVINE, SPAWN_ECRUTEAK, SPAWN_MAHOGANY, SPAWN_LAKE_OF_RAGE, SPAWN_BLACKTHORN, SPAWN_MT_SILVER
+
+	db SPAWN_PALLET, SPAWN_VIRIDIAN, SPAWN_PEWTER, SPAWN_CERULEAN, SPAWN_ROCK_TUNNEL, SPAWN_VERMILION, SPAWN_LAVENDER, SPAWN_SAFFRON, SPAWN_CELADON, SPAWN_FUCHSIA, SPAWN_CINNABAR
+
+	db -1
+
+unk_03f_4a5b:
+	call Call_03f_4a67
+	ldh a, [hROMBank]
+	ld hl, unkScript_03f_4a77
+	call FarQueueScript
+	ret
+
+Call_03f_4a67:
+	ld hl, unkData_03f_4a6e
+	call Call_03f_46df
+	ret
+
+unkData_03f_4a6e:
+	text "ワープします!" ; Warp!
+	done
+
+unkScript_03f_4a77:
+	applymovement PLAYER, .TeleportFrom
+	newloadmap MAPSETUP_TELEPORT
+	applymovement PLAYER, .TeleportTo
+	end
+
+.TeleportFrom:
+	teleport_from
+	step_end
+
+.TeleportTo:
+	teleport_to
+	step_end
+
+OTID_Editor:
+	call LoadStandardMenuHeader
+	call OTID_Init
+	call CloseWindow
+	ld a, 0
+	ret
+
+unk_03f_4a92:
+	ld a, 0
+	ret
+
+OTID_Init:
+; Clear OT ID input buffer
+	ld hl, wStringBuffer2
+	ld bc, 5
+	xor a
+	call ByteFill
+; Set default cursor position to last digit
+	ld a, 4
+	ld [wStringBuffer2 + 5], a
+; Print message
+	ld hl, NumberToChangeText
+	call PrintText
+
+.InitInputArea:
+	call OTID_UpdateInputArea
+
+.ReadJoypad:
+	call JoyTextDelay
+	ldh a, [hJoyLast]
+	ld b, a
+	and A_BUTTON
+	jr nz, .exit
+	ld a, b
+	and D_PAD
+	jr z, .ReadJoypad
+	call OTID_InterpretJoypad
+
+	ld c, 3
+	call DelayFrames
+	jr .InitInputArea
+
+.exit
+	call OTID_ConvertToHex
+	ld a, h
+	ld [wPartyMon1ID], a
+	ld a, l
+	ld [wPartyMon1ID + 1], a
+	ret
+
+NumberToChangeText:
+; “Which number should be changed?”
+	text "へんこうするナンバーを"
+	line "してい してください"
+	done
+
+OTID_UpdateInputArea:
+; Convert digit to character
+	hlcoord 14, 15
+	ld de, wStringBuffer2
+	ld c, 5
+.outputDigit
+	ld a, [de]
+	add "0"
+	ld [hli], a
+	inc de
+	dec c
+	jr nz, .outputDigit
+
+; Clear the cursor area
+	hlcoord 14, 16
+	ld bc, 5
+	ld a, " "
+	call ByteFill
+; Display cursor
+	hlcoord 14, 16
+	ld a, [wStringBuffer2 + 5]
+	ld e, a
+	ld d, 0
+	add hl, de
+	ld [hl], "▲"
+	ret
+
+OTID_InterpretJoypad:
+	ld a, b
+	and D_LEFT
+	jr nz, .pressed_left
+	ld a, b
+	and D_RIGHT
+	jr nz, .pressed_right
+	ld a, b
+	and D_UP
+	jr nz, .increment_digit
+	ld a, b
+	and D_DOWN
+	jr nz, .decrement_digit
+	ret
+
+.pressed_left
+	ld a, [wStringBuffer2 + 5]
+	; Are we on the left-most digit?
+	and a
+	ret z
+	; Nope, let's move left.
+	dec a
+	ld [wStringBuffer2 + 5], a
+	ret
+
+.pressed_right
+	ld a, [wStringBuffer2 + 5]
+	cp 4
+	ret z
+	inc a
+	ld [wStringBuffer2 + 5], a
+	ret
+
+.increment_digit
+	call OTID_GetDigitLocation
+	ld a, [hl]
+	cp 9
+	jr z, .max
+	inc a
+	ld [hl], a
+	ret
+
+.max
+	ld [hl], 0
+	ret
+
+.decrement_digit
+	call OTID_GetDigitLocation
+	ld a, [hl]
+	and a
+	jr z, .min
+	dec a
+	ld [hl], a
+	ret
+
+.min
+	ld [hl], 9
+	ret
+
+OTID_GetDigitLocation:
+	ld a, [wStringBuffer2 + 5]
+	ld e, a
+	ld d, 0
+	ld hl, wStringBuffer2
+	add hl, de
+	ret
+
+OTID_ConvertToHex:
+	ld hl, 0
+	ld de, wStringBuffer2 + 4
+	ld bc, 1
+	call .convertDigit
+	ld bc, 10
+	call .convertDigit
+	ld bc, 100
+	call .convertDigit
+	ld bc, 1000
+	call .convertDigit
+	ld bc, 10000
+; fallthrough
+
+.convertDigit:
+	ld a, [de]
+	dec de
+	push hl
+	ld hl, 0
+	call AddNTimes
+	ld c, l
+	ld b, h
+	pop hl
+	add hl, bc
+	ret
+
+unk_03f_4b91:
+	dr $fcb91,$fcba0
+
+unk_03f_4ba0:
+	dr $fcba0,$fcba9
+
+unk_03f_4ba9:
+	dr $fcba9,$fcbaf
+
+Call_03f_4baf:
+	dr $fcbaf,$fcbd5
+
+unk_03f_4bd5:
+	dr $fcbd5,$fcbdb
+
+Call_03f_4bdb:
+	dr $fcbdb,$fcc07
+
+Call_03f_4c07:
+	dr $fcc07,$fcc65
+
+unk_03f_4c65:
+	dr $fcc65,$fcc7a
+
+unk_03f_4c7a:
+	dr $fcc7a,$fccc0
+
+Call_03f_4cc0:
+	dr $fccc0,$fccd1
+
+unk_03f_4cd1:
+	dr $fccd1,$fcd51
+
+Call_03f_4d51:
+	dr $fcd51,$fcd59
+
+unk_03f_4d59:
+	dr $fcd59,$fcd81
+
+Call_03f_4d81:
+	dr $fcd81,$fcda7
+
+unk_03f_4da7:
+	dr $fcda7,$fcdae
+
+unk_03f_4dae:
+	dr $fcdae,$fcdb5
+
+unk_03f_4db5:
+	dr $fcdb5,$fcdbc
+
+unk_03f_4dbc:
+	dr $fcdbc,$fcebb
+
+Call_03f_4ebb:
+	dr $fcebb,$fcee2
